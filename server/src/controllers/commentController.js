3b098aafda07872fa013eac4d5e7266b9a6484e0
Ruslan <110040060+Deadusik@users.noreply.github.com>
2023-05-19 23:52:45 +0300

    fix problem in comment create methid

--- server/src/controllers/commentController.js
+++ server/src/controllers/commentController.js
@@ -3,19 +3,11 @@ const ApiError = require('../error/ApiError')
 const commentQueries = require('../queries/commentQueries')
 
 class CommentController {
-    async getAll(req, res) {
-        
-    }
-
     async create(req, res, next) {
         try {
             const { text, userId, parentCommentId } = req.body
 
-            console.log('comment data:', text, userId, parentCommentId)
-
-            pool.query(commentQueries.createComment(text, userId, parentCommentId), (error, results) => {
-                if(error) return next(ApiError.badRequest(error.message))
-            })
+            await pool.query(commentQueries.createComment(text, userId, parentCommentId))
 
             res.status(200).json({message: 'Коментар був створенний'})
         } catch(e) {
--- server/src/queries/commentQueries.js
+++ server/src/queries/commentQueries.js
@@ -1,6 +1,8 @@
+const moment = require('moment')
+
 const createComment = (text, userId, parentCommentId) => {
-    return `INSERT INTO "comment" (text, user_id, parent_comment_id) `
-    + `VALUES ('${text}', '${userId}', '${parentCommentId}')`
+    return `INSERT INTO "comment" (text, user_id, parent_comment_id, publish_date) `
+    + `VALUES ('${text}', '${userId}', '${parentCommentId}', '${moment().format('YYYY-MM-D')}')`
 }
 
 module.exports = {
